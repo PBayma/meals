35b32927b7106aacb82bedbf646779dce69983cb
Pedro Oliveira Bayma <pedro_bayma@outlook.com>
2021-03-15 20:16:16 -0300

    Sctructure of Meals App

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos Cozinhar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CategoriesScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
