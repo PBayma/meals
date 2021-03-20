c78063bf6fdb852f4fbd538ceffe80194517a281
Pedro Oliveira Bayma <pedro_bayma@outlook.com>
2021-03-20 20:44:18 -0300

    Creating meal and showing in categories_meals_screen

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   // final Category category;
@@ -9,13 +10,22 @@ class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Category category = ModalRoute.of(context).settings.arguments;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
+    // final categoryMeals = null;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category.title),
-      ),
-      body: Center(
-        child: Text('Receitas da categoria ${category.id}'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(category.title),
+        ),
+        body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return Text('${categoryMeals[index].title}');
+          },
+        ));
   }
 }
