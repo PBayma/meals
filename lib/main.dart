37ed5fb68218190a8f7925e562f033c1667cf171
Pedro Oliveira Bayma <pedro_bayma@outlook.com>
2021-03-24 15:56:56 -0300

    Creating tabs_screen, and TabBar menu

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
+import 'screens/tabs_screen.dart';
 
 import 'utils/app_routes.dart';
 
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
