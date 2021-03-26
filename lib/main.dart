a08456e09f5daa8538846fa5bb21156f6b691d3b
Pedro Oliveira Bayma <pedro_bayma@outlook.com>
2021-03-26 20:42:23 -0300

    Improving tab navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals/screens/categories_meals_screen.dart';
-import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'screens/tabs_screen.dart';
 
