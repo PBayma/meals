488b095e52645f52070ff43198fcb67c84d9fd00
Pedro Oliveira Bayma <pedro_bayma@outlook.com>
2021-04-04 14:08:26 -0300

    Creating settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/settings_screen.dart';
 import 'screens/tabs_screen.dart';
 
 import 'utils/app_routes.dart';
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
         AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
       },
 
       //Métodos para criação de rotas dinamicas e tratamento de rotas
